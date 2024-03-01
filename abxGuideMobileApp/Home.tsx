72c43032d649d3c64cd449fbed28d75705601fe9
Axel <70969800+Zuxulius@users.noreply.github.com>
2024-03-01 12:28:21 +0100

    create grid for green buttons on homepage

--- abxGuideMobileApp/Home.tsx
+++ abxGuideMobileApp/Home.tsx
@@ -1,12 +1,64 @@
 import Header from './Header';
-import { View, Text } from 'react-native';
+import { View, StyleSheet, Text, Pressable} from 'react-native';
 
 function Home() {
     return (
         <>
-            <Header></Header>
+            <Header />
+            <Text style={styles.h2}>Vanliga samhällsförvärvade infektioner</Text>
+            <Separator />
+            <View style={styles.greenButtonDiv}>
+                <Pressable style={styles.greenButton}>
+                    <Text style={styles.buttonText}>Idk</Text>
+                </Pressable>
+                <Pressable style={styles.greenButton}>
+                    <Text style={styles.buttonText}>Idk</Text>
+                </Pressable>
+                <Pressable style={styles.greenButton}>
+                    <Text style={styles.buttonText}>Idk</Text>
+                </Pressable>
+                <Pressable style={styles.greenButton}>
+                    <Text style={styles.buttonText}>Idk</Text>
+                </Pressable>
+            </View>
+            <Text style={styles.h2}>Intensivvårdskrävande infektioner</Text>
+            <Text style={styles.h2}>Snabbresurser</Text>
         </>
     )
-}
+};
+
+const Separator = () => <View style={styles.separator} />;
+
+const styles = StyleSheet.create({
+    h2: {
+        color: '#1F4D83',
+        fontSize: 14,
+        textAlign: 'center',
+        marginTop: 10,
+        fontWeight: 'bold'
+    },
+    separator: {
+        marginVertical: 8,
+        borderBottomColor: '#737373',
+        borderBottomWidth: StyleSheet.hairlineWidth,
+      },
+      greenButtonDiv: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        gap: 10
+      },
+      greenButton: {
+        flexBasis: '40%',
+        display: 'flex',
+        justifyContent: 'center',
+        backgroundColor: '#DCEAFF',
+        padding: 5,
+      },
+      buttonText: {
+        color: '#1F4D83',
+        fontWeight: 'bold'
+      }
+});
 
 export default Home;
